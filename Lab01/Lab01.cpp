807e7bccdd312db64b1ba5fd4110f0b8e07b9434
Kendall Bingham <binghamkl@umkc.edu>
2018-11-25 20:52:16 -0600

    Lab 01 Creation

---
+++ Lab01/Lab01.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	float distance, time, velocity;
+
+	cout << "Enter how many miles you are traveling " << endl;
+	cin >> distance;
+
+	cout << "How fast are you travling in mph? " << endl;
+	cin >> velocity;
+
+    // Calculate the time it takes here.
+	time = 0;
+
+	cout << endl;
+	cout << "At " << velocity << " mph it will take " << time << " hours to travel " << distance << " miles." << endl;
+
+}
